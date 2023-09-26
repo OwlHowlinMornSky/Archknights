6cf19018b897020ea9b391544a6238d998769015
Tyler Parret True <mysteryworldgod@outlook.com>
2023-09-26 19:40:43 +0800

    init

---
+++ AppWin32/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 AppWin32.rc 使用
+//
+#define IDC_APPWIN32                    101
+#define IDS_APP_TITLE                   102
+#define IDS_STRING104                   103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
