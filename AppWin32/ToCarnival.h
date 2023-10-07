3f70b812ee223abeb58143b17cfe2dc40f4ba3f3
Tyler Parret True <mysteryworldgod@outlook.com>
2023-10-07 14:49:22 +0800

    修改注释

--- AppWin32/ToCarnival.h
+++ AppWin32/ToCarnival.h
@@ -31,7 +31,7 @@ namespace AppWin32 {
 
 /**
  * @brief 创建 Carnival 实例的方法。其创建的是 CarnivalWin32，这样包装是为了避免扩散头文件。
- * @param r_window: 要管理的窗口 的 RenderWindow 指针，是 CarnivalWin32 构造需要的数据。
+ * @param hWnd: 要管理的窗口 的 句柄，是 CarnivalWin32 构造需要的数据。
  * @return 创建好的 Carnival 实例。
 */
 std::unique_ptr<GUI::ICarnival> crateCarnival(HWND hWnd);
