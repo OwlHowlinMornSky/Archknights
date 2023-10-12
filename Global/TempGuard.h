5f3ab678e4901d95b8c6fb9f1af77d394aa4c722
Tyler Parret True <mysteryworldgod@outlook.com>
2023-10-12 09:29:50 +0800

    添加注释

--- Global/TempGuard.h
+++ Global/TempGuard.h
@@ -22,25 +22,46 @@
 
 namespace ohms {
 
+/**
+ * @brief 临时获取所有权，并保护目标的旧有值，析构时自动还原旧值。
+ * @tparam _T: 任何可以无异常复制的类型。
+*/
 template<class _T>
 class TempGuard final {
 public:
-	TempGuard(_T& t) noexcept :
+	/**
+	 * @brief 构造函数，自动复制保留旧值。
+	 * @param t: 目标对象。
+	*/
+	explicit TempGuard(_T& t) noexcept :
 		m_target(t) {
 		m_old = t;
 	}
-	TempGuard(const TempGuard&) = delete;
-	TempGuard(TempGuard&&) = delete;
+	/**
+	 * @brief 析构函数，自动还原旧值。
+	*/
 	~TempGuard() noexcept {
 		m_target = m_old;
 	}
+
+	TempGuard(const TempGuard&) = delete;
 	TempGuard& operator=(const TempGuard&) = delete;
+
+	/**
+	 * @brief 临时修改。把目标的值变为n。
+	 * @param n: 临时值。
+	*/
 	void operator=(const _T& n) noexcept {
 		m_target = n;
 	}
+	/**
+	 * @brief 临时修改。把目标的值变为n。
+	 * @param n: 临时值。
+	*/
 	void operator=(_T&& n) noexcept {
 		m_target = n;
 	}
+
 protected:
 	_T& m_target;
 	_T m_old;
