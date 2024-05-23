5dd0906a53e68441750ec2f309f6de8404f2ee63
Tyler Parret True <mysteryworldgod@outlook.com>
2024-05-23 15:38:33 +0800

    添加文件头

--- Client/Models/tiny_obj_loader.h
+++ Client/Models/tiny_obj_loader.h
@@ -1,7 +1,8 @@
-/*
+﻿/*
 The MIT License (MIT)
 
 Copyright (c) 2012-2016 Syoyo Fujita and many contributors.
+Copyright (C) 2023-2024  Tyler Parret True
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
