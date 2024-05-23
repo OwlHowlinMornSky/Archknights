2eeba6de08b1a9042e1ff7a830f4cd00b389d3eb
Tyler Parret True <mysteryworldgod@outlook.com>
2024-05-23 17:27:48 +0800

    开始添加physics

---
+++ Client/Physics/IDetector.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace Physics {
+
+class IDetector {
+public:
+};
+
+}
---
+++ Client/Physics/IWorld.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "IDetector.h"
+#include "IBody.h"
+#include "IWall.h"
+
+#include <memory>
+
+namespace Physics {
+
+class IWorld {
+
+public:
+	virtual std::unique_ptr<IBody> CreateBodyCircle() = 0;
+
+	virtual std::unique_ptr<IDetector> CreateDetectorCircle() = 0;
+	virtual std::unique_ptr<IDetector> CreateDetectorTiles() = 0;
+
+	virtual std::unique_ptr<IWall> CreateWall() = 0;
+
+};
+
+}
